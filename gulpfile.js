c8db02fab9861a959d286eee24c654a30de16ab0
ilya panin <ilya.panin@gmail.com>
2019-06-19 10:26:11 +0300

    gulp build and watch from build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,68 @@
-const gulp = require('gulp');
+const  gulp = require('gulp');
+const  { watch, parallel } = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+
+function sassCompile(cb) {
+  gulp.src('./src/sass/style.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'));
+
+  cb();
+}
+
+function staticCompile(cb) {
+  gulp.src('./src/*.html')
+    .pipe(gulp.dest('./build'));
+
+  gulp.src('./src/img/*')
+    .pipe(gulp.dest('./build/img'));
+
+
+  cb();
+}
+
+
+const build = parallel(sassCompile, staticCompile);
+
 
 function serve(done) {
   browserSync.init({
     server: {
-        baseDir: "./src/"
+        baseDir: "./build/"
     }
   });
 
-  gulp.watch('./src/**/*', cb => {
-    console.log('changed');
+  watch(['./build/**/*'], cb => {
+    console.log('restarting ...');
     browserSync.reload();
     cb();
   });
 
+  watch(['./src/**/*'], cb => {
+    console.log('rebuilding ...');
+    build();
+    cb();
+  });
+
+
+
   done();
 }
 
+
+
+exports.build = build;
+
 exports.serve = serve;
 exports.default = serve;
+exports.sass = sassCompile;
+
+
+
+ 
+ 
+ 
