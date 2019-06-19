536b3f015fca081eb6e3ccab2c3ad996c7a1f4ee
ilya panin <ilya.panin@gmail.com>
2019-06-20 02:26:30 +0300

    много всякого

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,46 @@ const  gulp = require('gulp');
 const  { watch, parallel } = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
 
 sass.compiler = require('node-sass');
 
 
 function sassCompile(cb) {
   gulp.src('./src/sass/style.sass')
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/css'));
 
-  cb();
+  if (typeof cb === 'function')
+  {
+    cb();
+  }
 }
 
-function staticCompile(cb) {
+function htmlCompile(cb) {
   gulp.src('./src/*.html')
     .pipe(gulp.dest('./build'));
 
+  if (typeof cb === 'function')
+  {
+    cb();
+  }
+}
+
+function imgCompile(cb) {
   gulp.src('./src/img/*')
     .pipe(gulp.dest('./build/img'));
 
+  if (typeof cb === 'function')
+  {
+    cb();
+  }
 
-  cb();
 }
 
 
+const staticCompile = parallel(htmlCompile, imgCompile)
 const build = parallel(sassCompile, staticCompile);
 
 
@@ -36,18 +52,32 @@ function serve(done) {
     }
   });
 
+  // при любом изменении билда - рестарт
   watch(['./build/**/*'], cb => {
     console.log('restarting ...');
     browserSync.reload();
     cb();
   });
 
-  watch(['./src/**/*'], cb => {
-    console.log('rebuilding ...');
-    build();
+  build();
+
+  watch(['./src/**/*.sass'], cb => {
+    console.log('rebuilding sass...');
+    sassCompile();
     cb();
   });
 
+  watch(['./src/img/*'], cb => {
+    console.log('rebuilding img...');
+    imgCompile();
+    cb();
+  });  
+
+  watch(['./src/**/*.html'], cb => {
+    console.log('rebuilding html...');
+    htmlCompile();
+    cb();
+  });    
 
 
   done();
